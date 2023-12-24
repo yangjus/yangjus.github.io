1d70c315bd6845b2609558c403cce2f147acd21d
yangjus <yangjus@umich.edu>
2023-12-23 22:49:28 -0500

    added emailjs functionality

--- scripts.js
+++ scripts.js
@@ -8,3 +8,35 @@ function scrollFunction() {
     document.getElementsByTagName("nav")[0].style.backgroundColor = "beige";
   }
 }
+
+// Contact form
+const contactForm = document.querySelector("#contact-form");
+const submitBtn = document.querySelector(".submit-btn");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const messageInput = document.querySelector("#message");
+
+const publicKey = "oN2R7zaGKKb1hOHE1";
+const serviceID = "service_qnws2wi";
+const templateID = "template_wp1rcv3";
+
+emailjs.init(publicKey);
+contactForm.addEventListener("submit", e => {
+  e.preventDefault();
+  submitBtn.innerText = "Sending...";
+  const inputFields = {
+    name: nameInput.value,
+    email: emailInput.value,
+    message: messageInput.value
+  }
+  emailjs.send(serviceID, templateID, inputFields)
+    .then(() => {
+      submitBtn.innerText = "Message sent successfully";
+      nameInput.value = "";
+      emailInput.value = "";
+      messageInput.value = "";
+    }, (error) => {
+      console.log(error);
+      submitBtn.innerText = "Something went wrong";
+    });
+});
